a8895a3abc56138f2e85a48455d5c84aa15f5134
Lucas Leandro <lucasleandro159@icloud.com>
2022-06-21 23:33:57 -0300

    Atribuicao dos dados dos inputs em variaveis, criacao da funcao de calcular e acessando script.js no index

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let qtdAdultos = document.getElementById("adultos");
+let qtdCriancas = document.getElementById("criancas");
+let qtdHoras = document.getElementById("hora");
+
+function calcularQtd(){
+    
+}
